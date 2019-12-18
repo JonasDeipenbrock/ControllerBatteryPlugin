d81bc7ce0dd52078b37a08b68ef1220964c2e900
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2019-12-18 20:34:29 +0100

    Initial commit

---
+++ ControllerBatteryPlugin/Controllerbatteryplugin.cpp
@@ -0,0 +1,23 @@
+// ControllerBatteryplugin.cpp : Defines the exported functions for the DLL application
+//
+
+#include "ControllerBatteryPlugin.h"
+
+
+BAKKESMOD_PLUGIN(ControllerBatteryPlugin, "Controller Battery Plugin", "0.1", PLUGINTYPE_FREEPLAY)	//TODO: Update Plugin type
+
+
+/*
+	called when plugin is loaded, main command here
+*/
+void ControllerBatteryPlugin::onLoad()
+{
+
+}
+
+/*
+	called when plugin is unload, cleanup
+*/
+void ControllerBatteryPlugin::onUnload()
+{
+}
